6a7473b65f0d9d5898c8695103d7717a380b4109
KigoJomo <kigojomo@gmail.com>
2025-05-29 20:24:15 +0300

    fix: adjust form layout for better responsiveness and update Button component styling

--- app/slug-generator/components/SlugGen.tsx
+++ app/slug-generator/components/SlugGen.tsx
@@ -18,7 +18,7 @@ const SlugGen = () => {
   return (
     <div className="w-full flex flex-col gap-4">
       <form
-        className="w-full flex items-center gap-2"
+        className="w-full flex flex-col md:flex-row items-center gap-2"
         onSubmit={(e) => {
           e.preventDefault();
           setResult(slugify(input));
@@ -34,7 +34,7 @@ const SlugGen = () => {
           onChange={(e) => setInput(e.target.value)}
         />
 
-        <Button type="submit" onClick={() => setResult(slugify(input))}>
+        <Button type="submit" onClick={() => setResult(slugify(input))} className='w-full md:w-fit'>
           Generate
         </Button>
       </form>
